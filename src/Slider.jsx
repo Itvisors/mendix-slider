1cdc5c767c53c25f802db9cc2586e34d5ee2513e
PaulJ520 <73166341+PaulJ520@users.noreply.github.com>
2021-03-17 19:37:15 +0100

    Added comments in code
    Changed the test application and made some comments in the code

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -38,7 +38,12 @@ export default class Slider extends Component {
             onChangeAction.execute();
         }
     };
-
+   /**
+     * This function makes sure the slider cant be dragged over the min or max values
+     * @param {*} min The min value of the slider
+     * @param {*} max The max value of the slider
+     * @param {*} value The current value of the slider
+     */
     convertValue(min, max, value) {
         if (value < min) {
             value = min;
